e6b171bc9507aadb59d2d260da3cc168dd76295a
yangxinxiang <yangxinxiang@100tal.com>
2021-02-05 23:34:51 +0800

    feat: 接入rxjs来管理聪明组件(容器组件)中的数据状态

--- src/component/counter.js
+++ src/component/counter.js
@@ -3,25 +3,30 @@
  */
 import React, { Component } from "react";
 import ViewCounter from "./CounterView";
+import { Subject } from "rxjs";
+import { scan } from "rxjs/operators";
 export default class Counter extends Component{
     constructor(){
         super(...arguments);
         this.state = {
             count: 0
         }
+        this.counter = new Subject();
+        this.counter.pipe(
+            scan((acc, value)=> acc+value, 0)
+        ).subscribe(
+            currentCount => this.setState({...this.state, count:currentCount})
+        )
+        
         this.onIncreaseClick = this.onIncreaseClick.bind(this);
         this.onDecreaseClick = this.onDecreaseClick.bind(this);
     }
     
     onIncreaseClick(){
-        this.setState({
-            count : this.state.count+1
-        })
+        this.counter.next(1);
     }
     onDecreaseClick(){
-        this.setState({
-            count : this.state.count-1
-        })
+        this.counter.next(-1);
     }
     
     render(){
